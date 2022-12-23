2a475edb1ac201147fa70b5b9d02aaffd9cd0f5c
ragingpenguin <eflinjh@gmail.com>
2022-12-23 13:47:20 -0500

    added current time to the DOM using DAYJS

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+
+
+// GIVEN I am using a daily planner to create a schedule
+// WHEN I open the planner
+// THEN the current day is displayed at the top of the calendar
+// WHEN I scroll down
+// THEN I am presented with timeblocks for standard business hours
+// WHEN I view the timeblocks for that day
+// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+
+// use an if statement to check the current hour, and update the class of the time block so that it displays 
+// as the currect color
+
+
+// WHEN I click into a timeblock
+// THEN I can enter an event
+// WHEN I click the save button for that timeblock
+// THEN the text for that event is saved in local storage
+// WHEN I refresh the page
+// THEN the saved events persist
+$(document).ready (function () {
+  console.log("ready")
+  var currentDay = $("#currentDay");
+  var today = dayjs();
+  currentDay.text(today.format('MMM D, YYYY, hh:mm:ss'));
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+
+  
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id

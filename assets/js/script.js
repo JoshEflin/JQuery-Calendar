1568523737eb9c4fba82a8e0f3d04cf6a481ca27
ragingpenguin <eflinjh@gmail.com>
2023-01-03 13:29:16 -0500

    restructured repo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,30 @@
+// add in a function that either changes the color of the field  for events  that are alreadyscheduled   
 var saveBtn = $(".saveBtn");
 var timeBlockArr = $(".time-block");
 var resetBtn = $(".clear-button");
 
-for (i=0; i< timeBlockArr.length; i++){
-  var timeBlock= timeBlockArr[i];
-  var scheduledActivity =(JSON.parse(localStorage.getItem(timeBlock.id)));
-  console.log(scheduledActivity);
+for (i = 0; i < timeBlockArr.length; i++) {
+  var timeBlock = timeBlockArr[i];
+  var scheduledActivity = (JSON.parse(localStorage.getItem(timeBlock.id)));
   $(".description")[i].value = scheduledActivity;
 }
 
-//  .val(put get item inside )
+// display time  and assign colors to divs based on the time of day it is
 function displayTime() {
-  
   var currentDay = $("#currentDay");
   var today = dayjs();
   currentDay.text(today.format('MMM DD, YYYY  hh:mm '));
-  
+
   //  for  each item in class "time-block"
-  for (i=0; i< timeBlockArr.length; i++){
-    var timeBlock= timeBlockArr[i];
+  for (i = 0; i < timeBlockArr.length; i++) {
+    var timeBlock = timeBlockArr[i];
     var scheduledTime = timeBlock.id.slice(5);
-    console.log(timeBlock)
-    if (scheduledTime < today.$H){
-    timeBlock.className = "row time-block past";
-    }else if (scheduledTime == today.$H){
+
+    if (scheduledTime < today.$H) {
+      timeBlock.className = "row time-block past";
+    } else if (scheduledTime == today.$H) {
       timeBlock.className = "row time-block present";
-    }else if (scheduledTime> today.$H){
+    } else if (scheduledTime > today.$H) {
       timeBlock.className = "row time-block future";
     }
   }
@@ -34,11 +33,12 @@ function displayTime() {
 // sets local storage to the value of the text area at that time block when use presses save button
 function saveUserInput() {
   var textExtraction = $(this).siblings('.description')[0];
-  var textParent= $(this).parents()[0].id;
-  localStorage.setItem(textParent,JSON.stringify(textExtraction.value));
+  var textParent = $(this).parents()[0].id;
+  localStorage.setItem(textParent, JSON.stringify(textExtraction.value));
 }
+// call display time function  and set its interval to every minute
 displayTime();
-$(document).ready (setInterval(displayTime,60000));
+$(document).ready(setInterval(displayTime, 60000));
 
 resetBtn.on('click', function clearStorage() {
   localStorage.clear();

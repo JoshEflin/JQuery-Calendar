ffed5b4bff36a1f6170788ccfbc1a7bd280e119c
ragingpenguin <eflinjh@gmail.com>
2023-01-03 13:41:39 -0500

    reformat js file, added some comments for future reference

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,23 @@
-// add in a function that either changes the color of the field  for events  that are alreadyscheduled   
+// declar global variables, JQuery objects etc. 
 var saveBtn = $(".saveBtn");
 var timeBlockArr = $(".time-block");
 var resetBtn = $(".clear-button");
 
+// load page with local storage items inside of text areas if applicable
 for (i = 0; i < timeBlockArr.length; i++) {
   var timeBlock = timeBlockArr[i];
   var scheduledActivity = (JSON.parse(localStorage.getItem(timeBlock.id)));
   $(".description")[i].value = scheduledActivity;
 }
-
 // display time  and assign colors to divs based on the time of day it is
 function displayTime() {
   var currentDay = $("#currentDay");
   var today = dayjs();
   currentDay.text(today.format('MMM DD, YYYY  hh:mm '));
-
   //  for  each item in class "time-block"
   for (i = 0; i < timeBlockArr.length; i++) {
     var timeBlock = timeBlockArr[i];
     var scheduledTime = timeBlock.id.slice(5);
-
     if (scheduledTime < today.$H) {
       timeBlock.className = "row time-block past";
     } else if (scheduledTime == today.$H) {
@@ -29,7 +27,6 @@ function displayTime() {
     }
   }
 }
-
 // sets local storage to the value of the text area at that time block when use presses save button
 function saveUserInput() {
   var textExtraction = $(this).siblings('.description')[0];
@@ -39,10 +36,10 @@ function saveUserInput() {
 // call display time function  and set its interval to every minute
 displayTime();
 $(document).ready(setInterval(displayTime, 60000));
-
+// clear local storage and refresh page
 resetBtn.on('click', function clearStorage() {
   localStorage.clear();
   window.location.reload();
 });
-
+// event listener 
 saveBtn.on('click', saveUserInput);
